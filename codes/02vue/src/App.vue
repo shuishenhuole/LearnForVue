f24e008cab766dc9a6cf23ee9abf71e69717d2b4
shuishenhuole2024 <2120946311@qq.com>
2026-01-04 02:12:52 +0800

    新建了一个仓库并且完成了初始化

---
+++ codes/02vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>{{ message }}</h1>
+<button @click="incrment">increment</button>
+</template>
+
+<script setup lang="ts">
+import { nextTick, ref, shallowRef } from 'vue'
+   const message = ref(1)
+  async function incrment(){
+    message.value += 1
+    // await nextTick()
+   }
+</script>
+
+<style scoped>
+    .red{
+      color: red;
+    }
+</style>
